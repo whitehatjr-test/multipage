3c508f9c1ff7cb08c659f40385cd798ef577f6d0
whitehatjr-test <67689674+whitehatjr-test@users.noreply.github.com>
2021-07-20 19:56:52 +0530

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,48 +1,52 @@
+# S2.1: Design the Page 2 of the multipage app.
+# Import necessary modules 
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# # Define a function 'app()' which accepts 'car_df' as an input.
 def app(car_df):
     st.header('Visualise data')
     
-    car_df =car_df
-
+    # Add a multiselect widget with label 'Select the Charts/Plots:'
+    # and with 4 options passed as a tuple ('Correlation Heatmap', 'Scatter Plot', 'Box Plot', 'Histogram').
+    # Store the current value of this widget in a variable 'plot_list'.
     st.subheader("Visualisation Selector")
-    plot_list = st.multiselect("Select the Charts/Plots:",('Correlation Heatmap', 'Line Chart', 'Area Chart', 'Box Plot', 'Histogram'))
-      
+    plot_list = st.multiselect("Select the Charts/Plots:",
+                               ('Correlation Heatmap', 'Scatter Plot', 'Box Plot', 'Histogram'))
+
+    # Remove deprecation warning 
     st.set_option('deprecation.showPyplotGlobalUse', False)
 
-
+    # Display correlation heatmap using seaborn module and 'st.pyplot()'
     if 'Correlation Heatmap' in plot_list:
         st.subheader("Correlation Heatmap")
         sns.heatmap(car_df.corr(),annot=True)
         st.pyplot()
 
-    if 'Line Chart' in plot_list:
-        st.subheader("Line Chart")
-        lc_x = st.selectbox("Select the X-axis for Line chart",('enginesize','horsepower','carwidth','price'))
-        lc_y = st.selectbox("Select the Y-axis for Line chart",('enginesize','horsepower','carwidth','price'))
-        #st.line_chart(car_df[lc_x])
-        #st.write(car_df.index)
-        plt.plot(car_df[lc_x],car_df[lc_y])
-        st.pyplot()
-            
-    if 'Area Chart' in plot_list:
-        st.subheader("Area Chart")
-        ac_x = st.selectbox("Select the X-axis for Area chart",('enginesize','horsepower','carwidth','price'))
-        ac_y = st.selectbox("Select the Y-axis for Area chart",('enginesize','horsepower','carwidth','price'))
-        #st.area_chart(car_df[ac_x])
-        plt.stackplot(car_df[ac_x], car_df[ac_y])
+    # Display scatter plot
+    if 'Scatter Plot' in plot_list:
+        st.subheader("Scatter Plot")
+        sp_x = st.selectbox("Select the X-axis for Scatter Plot",('enginesize','horsepower','carwidth','price'))
+        sp_y = st.selectbox("Select the Y-axis for Scatter Plot",('enginesize','horsepower','carwidth','price'))
+        plt.scatter(car_df[sp_x], car_df[sp_y])
+        #plt.title("title")
+        plt.xlabel(sp_x)
+        plt.ylabel(sp_y)
         st.pyplot()
 
+       # Display box plot using seaborn module and 'st.pyplot()'
     if 'Box Plot' in plot_list:
         st.subheader("Box Plot")
-        bp = st.selectbox("Select the variable for boxplot",('enginesize','horsepower','carwidth','drivewheel','price'))
+        bp = st.selectbox("Select the variable for Box Plot",('enginesize','horsepower','carwidth','drivewheel','price'))
         sns.boxplot(car_df[bp])
         st.pyplot()
 
+    # Display histogram using matplotlib module and 'st.pyplot()'
     if 'Histogram' in plot_list:
         st.subheader("Histogram")
-        hg = st.selectbox("Select the variable for histogram",('enginesize','horsepower','carwidth','drivewheel','price'))
+        hg = st.selectbox("Select the variable for Histogram",('enginesize','horsepower','carwidth','drivewheel','price'))
         plt.hist(car_df[hg])
+        plt.xlabel(hg)
+        plt.ylabel("Frequency")
         st.pyplot()
