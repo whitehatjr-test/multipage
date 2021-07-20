2aa49873f9c80dcb9ec09a7075c8feade1e7ebef
whitehatjr-test <67689674+whitehatjr-test@users.noreply.github.com>
2021-07-20 19:55:08 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,51 +1,39 @@
 import streamlit as st
-import eda
-import plots
-import model
 import numpy as np
 import pandas as pd
 
+import data
+import plots
+import predict
 
 
-
-
-PAGES = {
-	"View Data": eda,
-    "Visualise Data": plots,
-    "Predict": model
-}
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
-@st.cache(persist=True)
+@st.cache()
 def load_data():
-# Read the dataset
+    # Read the dataset
     df = pd.read_csv("https://student-datasets-bucket.s3.ap-south-1.amazonaws.com/whitehat-ds-datasets/car-prices.csv")
-
-# Extract the name of the manufactures from the car names and display the first 25 cars to verify whether names are extracted successfully.
-    car_companies = pd.Series([car.split(" ")[0] for car in df['CarName']], index = df.index)
-
-# Create a new column named 'car_company'. It should store the company names of a the cars.
-    df['car_company'] = car_companies
-
 
 
     df = df[['enginesize','horsepower','carwidth','drivewheel','price']]
 
-    df['drivewheel'] = df['drivewheel'].map({'rwd': 0, 'fwd': 1, '4wd':2})
-    
-         
-
-        
+    df['drivewheel'] = df['drivewheel'].map({'rwd': 0, 'fwd': 1, '4wd':2})   
+                 
     return df
 
     
 car_df = load_data()
 
 st.title("Welcome to Car Price Prediction App")
-st.write("You can learn how to host a stream app on Heroku by watching this video:")
+
 with st.beta_expander("Watch Video"):
-	st.video("https://youtu.be/oBA5I__AfmY")
+    st.write("You can learn how to host a Streamlit app on Heroku by watching this video:")
+    st.video("https://youtu.be/oBA5I__AfmY")
+
+PAGES = {
+    "View Data": data,
+    "Visualise Data": plots,
+    "Predict": predict
+}
+
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
